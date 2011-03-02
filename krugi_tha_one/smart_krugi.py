946b649b291bb9db6df7b7d762122fa4f437d93e
Flurin Rindisbacher <info@flurischt.ch>
2011-03-02 22:38:42 +0100

    a more smarter one

--- krugi_tha_one/smart_krugi.py
+++ krugi_tha_one/smart_krugi.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from xgoogle.translate import LanguageDetector, DetectionError, Translator, TranslationError
 from krugi_constants import *
-import tweepy, time, pickle, wolframalpha
+import tweepy, time, pickle, wolframalpha, random, sys, traceback
 
-WHITELIST = []
+WHITELIST = ['flurischt','steffstefferson','webair84','krigu_tha_one', 'buergich']
 OWN_NAME = 'krugi_tha_one'
 PICKLE_FILE = 'data.pkl'
+COUNTER_QUESTIONS = [ 'I\'m not sure what you mean...', 'Is this rhetorical?', 'yes', 'nope', 'Do YOU know this?', 'what????', 'haha, this is too easy for me...', 'How should I know that?? I\'m not IBM\'s Watson!' ]
 
+def formatExceptionInfo(maxTBlevel=5):
+    cla, exc, trbk = sys.exc_info()
+    excName = cla.__name__
+    try:
+        excArgs = exc.__dict__["args"]
+    except KeyError:
+        excArgs = "<no args>"
+    excTb = traceback.format_tb(trbk, maxTBlevel)
+    return (excName, excArgs, excTb)
+    
 class Smart_Krugi():
 
     def __init__(self, api):
         self.api = api
-        self.detect = LanguageDetector().detect
-        self.translate = Translator().translate
 
     def listen(self, api):
         last_id = self.recover()
@@ -30,7 +38,7 @@ class Smart_Krugi():
             
             self.backup(last_id)
             # at maximum 350 requests per hour !!!
-            time.sleep(55)
+            time.sleep(30)
 
     def recover(self):
         pkl_file = open(PICKLE_FILE, 'rb')
@@ -50,26 +58,38 @@ class Smart_Krugi():
         print 'got a message from @' + username + ':', status.text
         if username != OWN_NAME and username in WHITELIST:
             text = status.text.lower().replace('@'+OWN_NAME,'')
-            lang = self.detect(text)
-            if lang.lang_code != 'en':
-                text = self.translate(text)
             
             # ask wolfram
             query = wolframalpha.WolframAlpha(text)
             
             if len(query.results) >= 2:
-                answer = query.results[1].result_raw.replace('Wolfram|Alpha','me')
-                self.reply(status,answer)
-                return
+                answer = query.results[1].result_raw.replace('Wolfram|Alpha',OWN_NAME)
+            else:
+                if '?' in status.text:
+                    if 'clothes' in status.text:
+                        answer = 'me julie went shopping for me... http://www.youtube.com/watch?v=JB6YL1ko8_s'
+                    else:
+                        answer = COUNTER_QUESTIONS[random.randint(0,len(COUNTER_QUESTIONS)-1)]
+                else:
+                    if 'funny' in status.text or 'brilliant' in status.text or 'intelligent' in status.text or 'smart' in status.text:
+                        answer = 'thank you!'
+                    else:
+                        answer = 'Check your spelling, and use English!! (Errcode ' + str(random.randint(0,10000)) + ')'
 
-            #self.reply(status, 'shut up!')
+            self.reply(status,answer)
+
 
     def reply(self,status,answer):
+        message = '@'+status.user.screen_name+' '+answer
+        message.replace('\n', ' ')
+        if len(message) > 140:
+            message = message[0:139]
+            print message
         try:
-            self.api.update_status(answer+' @'+status.user.screen_name,status.id)
+            self.api.update_status(message,status.id)
+            print message
         except tweepy.TweepError:
-            pass
-        print answer, '@'+status.user.screen_name 
+            print formatExceptionInfo()
 
 if __name__ == '__main__':
     # dirty workaraound for the unicode problems
