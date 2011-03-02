338b694f9ab15721bb798f95bcf28412a62f73ad
Flurin Rindisbacher <info@flurischt.ch>
2011-03-02 16:50:25 +0100

    smart and foreign language capable krugi

--- krugi_tha_one/smart_krugi.py
+++ krugi_tha_one/smart_krugi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#from xgoogle.translate import LanguageDetector, DetectionError, Translator, TranslationError
+from xgoogle.translate import LanguageDetector, DetectionError, Translator, TranslationError
 from krugi_constants import *
 import tweepy, time, pickle, wolframalpha
 
-WHITELIST = ['flurischt','steffstefferson']
+WHITELIST = []
 OWN_NAME = 'krugi_tha_one'
 PICKLE_FILE = 'data.pkl'
 
@@ -30,7 +30,7 @@ class Smart_Krugi():
             
             self.backup(last_id)
             # at maximum 350 requests per hour !!!
-            time.sleep(20)
+            time.sleep(55)
 
     def recover(self):
         pkl_file = open(PICKLE_FILE, 'rb')
@@ -47,31 +47,35 @@ class Smart_Krugi():
 
     def process(self, status):
         username = status.user.screen_name.lower()
+        print 'got a message from @' + username + ':', status.text
         if username != OWN_NAME and username in WHITELIST:
             text = status.text.lower().replace('@'+OWN_NAME,'')
-            #lang = self.detect(text)
-            #if lang.lang_code != 'en':
-            #    text = self.translate(text)
-
+            lang = self.detect(text)
+            if lang.lang_code != 'en':
+                text = self.translate(text)
+            
             # ask wolfram
             query = wolframalpha.WolframAlpha(text)
-            #for rs in query.results:
-            #    if 'result' in rs.title.lower():
-                    #self.reply(status,self.translate(rs.result, lang.lang_code))
+            
             if len(query.results) >= 2:
-                self.reply(status,query.results[1].result)
+                answer = query.results[1].result_raw.replace('Wolfram|Alpha','me')
+                self.reply(status,answer)
                 return
 
-            self.reply(status, 'shut up!')
+            #self.reply(status, 'shut up!')
 
     def reply(self,status,answer):
         try:
             self.api.update_status(answer+' @'+status.user.screen_name,status.id)
-        except TweepError:
+        except tweepy.TweepError:
             pass
-        print answer 
+        print answer, '@'+status.user.screen_name 
 
 if __name__ == '__main__':
+    # dirty workaraound for the unicode problems
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     api = tweepy.API(auth)
