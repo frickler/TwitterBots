e417211240a1a1a2dba0e524ca1f3ebc862dca6c
Flurin Rindisbacher <info@flurischt.ch>
2011-03-03 22:14:39 +0100

    a major rewrite of smart_krugi

--- krugi_tha_one/smart_krugi.py
+++ krugi_tha_one/smart_krugi.py
@@ -4,10 +4,41 @@
 from krugi_constants import *
 import tweepy, time, pickle, wolframalpha, random, sys, traceback
 
-WHITELIST = ['flurischt','steffstefferson','webair84','krigu_tha_one', 'buergich']
+## SETTINGS ##
 OWN_NAME = 'krugi_tha_one'
+THE_OWNER = 'flurischt'
 PICKLE_FILE = 'data.pkl'
-COUNTER_QUESTIONS = [ 'I\'m not sure what you mean...', 'Is this rhetorical?', 'yes', 'nope', 'Do YOU know this?', 'what????', 'haha, this is too easy for me...', 'How should I know that?? I\'m not IBM\'s Watson!' ]
+DEBUG = True
+
+## some phrases that are matched by keyword ##
+KEYWORD_PHRASES = [
+    ( 'clothes', 'me julie went shopping for me... http://www.youtube.com/watch?v=JB6YL1ko8_s' ),
+    ( 'brilliant', 'thank you!' ),
+    ( 'intelligent', 'yeah, thanks!' ),
+    ( 'smart', 'thx!' )
+]
+
+## possible answers to a question ##
+COUNTER_QUESTIONS = [ 
+    'I\'m not sure what you mean...', 
+    'Is this rhetorical?', 
+    'yes', 
+    'nope', 
+    'do YOU know this?', 
+    'what????', 
+    'haha, this is too easy for me...', 
+    'How should I know that?? I\'m not IBM\'s Watson!', 
+    'only @flurischt could answer this! ;-)' 
+]
+
+## random answers if everything else failed ##
+RANDOM_PHRASES = [ 'shut up!', 'Check your spelling, and use English!!' ]
+
+## the following keywords will always be hidden in krugis answer ##
+HIDE = [
+    ( 'Wolfram|Alpha', OWN_NAME ),
+    ( 'Stephen Wolfram', 'a frickler' ),
+]
 
 def formatExceptionInfo(maxTBlevel=5):
     cla, exc, trbk = sys.exc_info()
@@ -23,9 +54,14 @@ class Smart_Krugi():
 
     def __init__(self, api):
         self.api = api
+        self.refresh_whitelist()
+
+    def refresh_whitelist(self):
+        self.WHITELIST = ['flurischt','steffstefferson','webair84','krigu_tha_one', 'buergich']
 
     def listen(self, api):
         last_id = self.recover()
+        print 'DEBUG: %s' % DEBUG
         print 'recovered:', last_id
         while(True):
             if last_id:
@@ -55,38 +91,51 @@ class Smart_Krugi():
 
     def process(self, status):
         username = status.user.screen_name.lower()
-        print 'got a message from @' + username + ':', status.text
-        if username != OWN_NAME and username in WHITELIST:
+        print 'got a message from @%s : %s' % (username,status.text)
+        if username != OWN_NAME and username in self.WHITELIST:
             text = status.text.lower().replace('@'+OWN_NAME,'')
             
             # ask wolfram
+            answer = None
             query = wolframalpha.WolframAlpha(text)
-            
+                       
             if len(query.results) >= 2:
-                answer = query.results[1].result_raw.replace('Wolfram|Alpha',OWN_NAME).replace('Stephen Wolfram', 'a frickler')
+                answer = query.results[1].result_raw
             else:
-                if '?' in status.text:
-                    if 'clothes' in status.text:
-                        answer = 'me julie went shopping for me... http://www.youtube.com/watch?v=JB6YL1ko8_s'
-                    else:
-                        answer = COUNTER_QUESTIONS[random.randint(0,len(COUNTER_QUESTIONS)-1)]
+                # wolfram had no idea...
+                for (k,v) in KEYWORD_PHRASES:
+                    if k in text:
+                        answer = v
+                        break
+
+                if not answer and '?' in text:
+                    answer = COUNTER_QUESTIONS[random.randint(0,len(COUNTER_QUESTIONS)-1)]
                 else:
-                    if 'funny' in status.text or 'brilliant' in status.text or 'intelligent' in status.text or 'smart' in status.text:
-                        answer = 'thank you!'
-                    else:
-                        answer = 'Check your spelling, and use English!! (Errcode ' + str(random.randint(0,10000)) + ')'
+                    answer = '%s (Errcode %i)' % (RANDOM_PHRASES[random.randint(0,len(RANDOM_PHRASES)-1)], random.randint(0,10000))
 
             self.reply(status,answer)
 
+    def process_dbg(self, text):
+        user = tweepy.User()
+        status = tweepy.Status() 
+        user.screen_name = 'flurischt'
+        status.id = 99999
+        status.text = text
+        status.user = user
+        self.process(status)
 
     def reply(self,status,answer):
-        message = '@'+status.user.screen_name+' '+answer
-        message.replace('\n', ' ')
+        # remove the "forbidden" keywords 
+        for (k,v) in HIDE:
+            answer.replace(k, v)
+        
+        message = '@%s %s' % (status.user.screen_name, answer)
+
         if len(message) > 140:
             message = message[0:139]
-            print message
         try:
-            self.api.update_status(message,status.id)
+            if not DEBUG:
+                self.api.update_status(message,status.id)
             print message
         except tweepy.TweepError:
             print formatExceptionInfo()
@@ -104,4 +153,4 @@ if __name__ == '__main__':
     try:
         smart.listen(api)
     except KeyboardInterrupt:
-        print 'bye!'
+        print '\nbye!'
