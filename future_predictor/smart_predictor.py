4dc2c5c7745ce5c2ca8fe097dc7d687130893cbd
steffstefferson <inf.stef.kaeser@gmail.com>
2011-03-22 23:45:10 +0100

    sdasd

--- future_predictor/smart_predictor.py
+++ future_predictor/smart_predictor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#from krugi_constants import *
+from predictor_constants import *
 import tweepy, time, pickle, random, sys, traceback
 import logic
 import storage
